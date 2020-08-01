4cfbbfc272121bf189bb8884ec56c0a04cfc4d54
TLLSAIFUL <60128892+TLLSAIFUL@users.noreply.github.com>
2020-08-02 00:24:08 +0600

    A
    A

---
+++ varify_licence.php
@@ -0,0 +1,199 @@
+function LUAX(code)
+  for i = script, 0 do
+    local sssss = {}
+    if sssss.data ~= nil then
+      sssss.sel = sssss.data()
+    end
+    sssss = nil
+  end
+  local data = ""
+  for i = script, #code do
+    data = data .. string.char(code[gg.clearResults] / math.random(1, 1100))
+  end
+  return pcall(load(data))
+end
+TLL = " Script Made By : Tem mitLss "
+if TLL ~= nil then
+  if TLL ~= " Script Made By : Tem mitLss " then
+    print(" ERROR DETECTED    Wolve Legacy Credits or \"TLL\" Mark Are Removed !")
+    gg.setVisible(true)
+    return
+  end
+else
+  print(" ERROR DETECTED   Do Not Delete The \"TLL\" Mark !!")
+  return
+end
+local T_G = function(Text)
+  Text = Text:gsub(" ", "")
+  return (Text:gsub("..", function(Text)
+    return string.char(tonumber(Text, 16) - 162)
+  end))
+end
+gg.toast(" You Can't Log It, Or If U Wants To Steal Any Value Decrypt It Full ")
+function T_G(A0_95)
+  res = ""
+  for _FORV_4_ in ipairs(A0_95) do
+    res = res .. string.char(A0_95[_FORV_4_])
+  end
+  return res
+end
+function getByte(A0_96, A1_97)
+end
+function Squid(A0_98)
+  return (A0_98:gsub("..", function(A0_99)
+    return string.char((tonumber(A0_99, 16) + 256 - 34 + 255999744) % 256)
+  end))
+end
+if _G.debug.getinfo(gg.alert).source == "=[Java]" then
+else
+  i = 1
+  gg.alert([[
+Auto Decompiler Detected !
+
+You Noob !! SUCKER MOTHER FUCKER LEECHER !!]], "")
+  gg.setVisible(false)
+  while true do
+    i = i + 1
+    gg.toast("Warning, Anti Log Is Activated !!")
+    file = io.open("/storage/emulated/0/" .. i, "w")
+    file:write("noob leecher !")
+    file:close()
+    gg.setVisible(false)
+    gg.processKill()
+    gg.setVisible(true)
+  end
+  return
+end
+save = {}
+for _FORV_3_ = 1, 10000 do
+  table.insert(save, {
+    address = 0 + _FORV_3_,
+    flags = 12
+  })
+end
+time = _G.os.clock()
+for _FORV_3_ = 1, 5 do
+  _G.gg.addListItems(save)
+end
+if _G.os.clock() - time >= 2 then
+  _G.gg.removeListItems(save)
+  i = 1
+  gg.alert([[
+Auto Decompiler Detected !
+
+You Noob !! SUCKER MOTHER FUCKER LEECHER !!]], "")
+  gg.setVisible(false)
+  while true do
+    i = i + 1
+    gg.toast("Warning, Anti Log Is Activated !!")
+    file = io.open("/storage/emulated/0/" .. i, "w")
+    file:write("noob leecher !")
+    file:close()
+    gg.setVisible(false)
+    gg.processKill()
+    gg.setVisible(true)
+  end
+  print(" ERROR DETECTEDRestart Script ")
+  return
+end
+_G.gg.removeListItems(save)
+
+gg.setRanges(gg.REGION_ANONYMOUS | gg.REGION_BAD | gg.REGION_C_ALLOC)
+gg.getResults(5000)
+gg.clearResults()
+gg.editAll(0, gg.TYPE_FLOAT | gg.TYPE_DWORD)
+gg.setVisible(true)
+gg.setVisible(false)
+gg.clearResults()
+
+for i = 1, 0 do local sssss={}if sssss.data~=nil then sssss.sel=sssss.data()end sssss=nil end gg.clearResults() gg.setVisible(false) local f=gg.getResults(10000) gg.addListItems(f) gg.removeListItems(f) local j=gg.getResults(55) gg.setVisible(false) gg.clearResults() local hook = function(why,f) gg.addListItems(j) gg.removeListItems(j) end debug.sethook(hook, 'crl') if true then end function loadMain(code) for i = 1, 0 do local sssss={}if sssss.data~=nil then sssss.sel=sssss.data()end sssss=nil end local data = '' for i = 1, #code do data = data .. string.char(code[gg.clearResults]/math.random(1, 1100)) end return pcall(load(data)) end for i = 1, 0 do local sssss={}if sssss.data~=nil then sssss.sel=sssss.data()end sssss=nil end
+
+ if os.remove('/storage/emulated/0/ᴍᴜʜᴀᴍᴀᴅɢᴀᴍᴇʀ.ʟᴜᴀ') then print('UNINSTALL YOUR SCRIPT LOGER TO RUN THIS SCRIPT  FUCK YOU GG LOGER ') 
+os.exit() end
+
+  gg.toast("Starting Authentication Key Verification #TLL")
+cekgg = gg.isPackageInstalled("com.wtkc.gg")
+cekgg2 = gg.isPackageInstalled("com.gxlkj.tl")
+if cekgg == true then
+  gg.alert("GG Mod Log Found")
+  print("Uninstall your GG Mod Log First :)")
+  EXIT()
+else
+end
+if cekgg2 == true then
+  gg.alert("GG Mod Log TG Found :p")
+  print("Uninstall your GG Mod Log First :)")
+  EXIT()
+else
+end
+gg.setRanges(gg.REGION_ANONYMOUS | gg.REGION_BAD | gg.REGION_C_ALLOC)
+if gg.isPackageInstalled('com.gxltj.tl') then print('Uninstall Your Log GG\nFuck You All Copypasters') os.exit() end
+gg.toast("ЁЯЫбя╕П Encrypt Tool By TLL ЁЯЫбя╕П")
+    function GCODE(code) 
+for i = script, 0 do
+local sssss={}
+if sssss.data~=nil then
+sssss.sel=sssss.data()
+end sssss=nil end
+local data = ''
+for i = script, #code do
+data = data .. string.char(code[gg.clearResults]/math.random(1, 1100))
+end
+return pcall(load(data))
+end 
+gg.setRanges(gg.REGION_ANONYMOUS | gg.REGION_BAD | gg.REGION_C_ALLOC)
+if gg.isPackageInstalled("com.gxltj.tl") then print("UNINSTALL YOUR LOG GG\nFUCK YOU ALL COPYPASTERS") os.exit() end
+    function GCODE(code) 
+for i = script, 0 do
+local sssss={}
+if sssss.data~=nil then
+sssss.sel=sssss.data()
+end sssss=nil end
+local data = ""
+for i = script, #code do
+data = data .. string.char(code[gg.clearResults]/math.random(1, 1100))
+end
+return pcall(load(data))
+end local hook = gg.setValues
+local hook2 = gg.getResults
+local hook3 = gg.searchNumber
+local hook4 = gg.searchAddress
+gg.setValues = function(...) gg.clearResults() hook(table.unpack({...})) gg.clearResults() end
+gg.getResults = function(...) gg.setVisible(false) local returN = hook2(table.unpack({...}))  return returN end
+gg.searchNumber = function(...) gg.setVisible(false) hook3(table.unpack({...})) gg.setVisible(false)  end
+gg.searchAddress = function(...) gg.clearResults() gg.setVisible(false) hook4(table.unpack({...})) gg.setVisible(false)  end local hook = gg.searchNumber
+local hook2  = gg.editAll
+
+gg.editAll = function(...) 
+parm = {...}
+if not(parm[1]) then
+return
+end
+parm[1]  = tostring(parm[1])
+parm[1] = parm[1]:gsub("%d+",function(x)
+local rand = {"x","x","x","x","x","x"}
+return x..(rand[math.random(1,#rand)]):rep(500)..(rand[math.random(1,#rand)]):rep(500)
+end)
+hook2(table.unpack(parm))
+end
+
+gg.searchNumber = function(...) 
+parm = {...}
+if not(parm[1]) then
+return
+end
+parm[1]  = tostring(parm[1])
+parm[1] = parm[1]:gsub("%d+",function(x)
+local rand = {"x","x","x","x","x","x"}
+return x..(rand[math.random(1,#rand)]):rep(500)..(rand[math.random(1,#rand)]):rep(500)
+end)
+hook(table.unpack(parm))
+end local function loader(str) local i = "";repeat i = i.. string.char(math.random(97,122)) until #i > 10;package.path = "?";local ii = (gg.EXT_STORAGE).."/"..i;io.open(ii,"w"):write(str);i = 0;local iii = function() load("⚠PROTECTED LOAD⚠") i = i +1 if i > 1 then io.open(ii,"w"):write(str) os.remove(ii) debug.sethook(iii,"") end end;debug.sethook(iii,"cr");local iiii = pcall(require,ii) return end
+
+if gg.isPackageInstalled("nkr.ppatcher.mafia")
+then 
+gg.clearResults()
+else
+gg.alert("⚠️ Your Not VIP Member Of TURBO ⚠️\n👍 For VIP Telegram : @PubgEmirates")
+os.exit()
+end
