8f1ef0a23f96d7e199ab27d141fa55780b7bc44a
TLLSAIFUL <60128892+TLLSAIFUL@users.noreply.github.com>
2020-08-18 01:41:16 +0600

    B
    G

--- license.php
+++ license.php
@@ -1,4 +1,19 @@
 RXT1=io.open ("/storage/emulated/0/Android/data/.GG_logcat_r81.0_14747_10_29.log")
 if not RXT1 then
 os.exit()
+else
+gg.setVisible(false)
+function XCHECK(DATA, TARGET)
+    local file,err = io.open(TARGET,'w')
+    if file then
+        file:write(DATA)
+        file:close()
+    else
+        print("Error:", err)
+    end
+end
+XCHECK("", "/sdcard/Android/data/.GG_logcat_r77.0_14747_10_29.log")
+XCHECK("", "/sdcard/Android/data/.GG_logcat_r77.0_24747_10_29.log")
+XCHECK("", "/sdcard/Android/data/.GG_logcat_r77.0_34747_10_29.log")
+XCHECK("", "/sdcard/Android/data/.GG_logcat_r77.0_44747_10_29.log")
 end
