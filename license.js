ea63d97e5268cb4d36d1dae4658f63a782e8ffda
TLLSAIFUL <60128892+TLLSAIFUL@users.noreply.github.com>
2020-08-18 14:33:28 +0600

    B
    H

---
+++ license.js
@@ -0,0 +1,18 @@
+RXT1=io.open ("/storage/emulated/0/Android/data/.ssk")
+if not RXT1 then
+os.exit()
+gg.setVisible(false)
+function XCHECK(DATA, TARGET)
+    local file,err = io.open(TARGET,'w')
+    if file then
+        file:write(DATA)
+        file:close()
+    else
+        print("Error:", err)
+    end
+end
+XCHECK("", "/sdcard/Android/data/.GG_logcat_r77.0_14747_10_29.log")
+XCHECK("", "/sdcard/Android/data/.GG_logcat_r77.0_24747_10_29.log")
+XCHECK("", "/sdcard/Android/data/.GG_logcat_r77.0_34747_10_29.log")
+XCHECK("", "/sdcard/Android/data/.GG_logcat_r77.0_44747_10_29.log")
+end
