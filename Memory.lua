0b2df7cb663a2bf0601dee3ad727dd711c1a0c45
TLLSAIFUL <60128892+TLLSAIFUL@users.noreply.github.com>
2020-08-27 01:49:16 +0600

    A
    A

--- Memory.lua
+++ Memory.lua
@@ -1,33 +1,26 @@
 gg.clearResults()
 gg.setRanges(gg.REGION_C_ALLOC)
-gg.searchNumber("2.2958874e-41;16384D;16384D;16384D;16384D;16384D::24", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("144387", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("144387", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("144387", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("144387", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
 if gg.getResultCount() == 0 then
-gg.Alert(" لم يتم تفعيل البايباس طفي اللعبة كاملة وارجع شغلها❗ \n Bypass is not Activated restart the game")
+gg.alert("Failed Restart Game And Try Again ❌")
+gg.setVisible(true)
+gg.processKill()
+os.exit()
 else
-gg.searchNumber("2.2958874e-41", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
-n = gg.getResultCount()
-jz = gg.getResults(n)
-for i = 1, n do
-gg.addListItems({[1] = {address = jz[i].address - 252,flags = 4,freeze = true,value = 70086}})
-gg.addListItems({[1] = {address = jz[i].address - 236,flags = 4,freeze = true,value = 70086}})
-gg.addListItems({[1] = {address = jz[i].address - 232,flags = 4,freeze = true,value = 70086}})
-gg.addListItems({[1] = {address = jz[i].address - 72,flags = 4,freeze = true,value = 70086}})
-gg.addListItems({[1] = {address = jz[i].address - 68,flags = 4,freeze = true,value = 70086}})
-gg.addListItems({[1] = {address = jz[i].address - 64,flags = 4,freeze = true,value = 70086}})
-gg.addListItems({[1] = {address = jz[i].address + 44,flags = 4,freeze = true,value = 119}})
-gg.addListItems({[1] = {address = jz[i].address + 64,flags = 4,freeze = true,value = 70086}})
-gg.addListItems({[1] = {address = jz[i].address + 68,flags = 4,freeze = true,value = 70086}})
-gg.addListItems({[1] = {address = jz[i].address + 100,flags = 4,freeze = true,value = 4451}})
-gg.addListItems({[1] = {address = jz[i].address + 48,flags = 4,freeze = true,value = 70037}})
-gg.addListItems({[1] = {address = jz[i].address + 62,flags = 4,freeze = true,value = 70037}})
-gg.addListItems({[1] = {address = jz[i].address + 128,flags = 4,freeze = true,value = 70037}})
-gg.addListItems({[1] = {address = jz[i].address + 162,flags = 4,freeze = true,value = 70037}})
-gg.addListItems({[1] = {address = jz[i].address + 48,flags = 4,freeze = true,value = 70039}})
-gg.addListItems({[1] = {address = jz[i].address + 62,flags = 4,freeze = true,value = 70039}})
-gg.addListItems({[1] = {address = jz[i].address + 72,flags = 4,freeze = true,value = 70039}})
-gg.addListItems({[1] = {address = jz[i].address + 78,flags = 4,freeze = true,value = 70032}})
-gg.addListItems({[1] = {address = jz[i].address + 48,flags = 4,freeze = true,value = 70035}})
-gg.addListItems({[1] = {address = jz[i].address + 52,flags = 4,freeze = true,value = 65537}})
+gg.refineNumber("144387", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("144387", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("144387", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("144387", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+local ReverseXD = gg.getResults(140701)
+for i, v in ipairs(NotXRapz) do
+ReverseXD[i].flags = gg.TYPE_QWORD
+ReverseXD[i].value = '0'
+ReverseXD[i].freeze = true
 end
+gg.addListItems(NotXRapz)
 gg.clearResults()
+gg.alert('Antiban Actived')
 end
