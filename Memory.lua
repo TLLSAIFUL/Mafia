7ec1f42d674121512bbd096b9b392ea386576130
TLLSAIFUL <60128892+TLLSAIFUL@users.noreply.github.com>
2020-09-18 20:12:43 +0600

    H
    G

--- Memory.lua
+++ Memory.lua
@@ -1,51 +1,251 @@
+gg.setVisible(false)
 gg.clearResults()
 gg.setRanges(gg.REGION_C_ALLOC)
-gg.searchNumber("67109377", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
-    gg.refineNumber("67109377", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
-    revert = gg.getResults(30101996, nil, nil, nil, nil, nil, nil, nil, nil)
-    for _FORV_4_, _FORV_5_ in ipairs(revert) do
-      if _FORV_5_.flags == gg.TYPE_DWORD then
-        _FORV_5_.value = "0"
-        _FORV_5_.freeze = true
-      end
-    end
-    gg.addListItems((revert))
-    gg.clearResults()
-	 gg.clearResults()
-    gg.setRanges(gg.REGION_C_ALLOC)
-    gg.searchNumber("909391408;808923191::8",gg.TYPE_DWORD)
-    gg.getResults(999)
-    gg.editAll("1089886885",gg.TYPE_DWORD)
-    gg.clearResults()
-    gg.setRanges(gg.REGION_C_ALLOC)
-    gg.searchNumber("909391408",gg.TYPE_DWORD)
-    gg.getResults(999)
-    gg.editAll("1089886885",gg.TYPE_DWORD)
-gg.clearResults()
-    gg.setRanges(4)
-    gg.searchNumber("2062", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
-    gg.searchNumber("2062", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
-    jg = gg.getResults(10000)
-    sl = gg.getResultCount()
-    for _FORV_3_ = tonumber("1"), sl do
-      dzy = jg[_FORV_3_].address
-      gg.addListItems({
-        [1] = {
-          address = dzy,
-          flags = gg.TYPE_DWORD,
-          freeze = true,
-          value = tonumber("0")
-        }
-      })
-      end
-	 gg.clearResults()
-	 gg.setRanges(4)
-	 gg.searchNumber("1,065,336,439D;1,065,353,216D;1,074,790,406D;1,073,741,824D;1,669,365,766D", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
-	 gg.searchNumber("1,065,336,439D;1,065,353,216D;1,074,790,406D;1,073,741,824D;1,669,365,766D", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
-	 jg=gg.getResults(100)
-	 sl=gg.getResultCount()
-	 for i = 1, sl do
-	 	 dzy=jg[i].address
-	 	 gg.addListItems({[1] = {address = dzy,flags = gg.TYPE_DWORD,freeze = true,value = 0}})
-	 	   gg.toast('𝐀𝐂𝐓𝐈𝐕𝐀𝐓𝐄𝐃')
+gg.searchNumber("133378", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("133378", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("133378", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+local Reverse = gg.getResults(140701)
+for i, v in ipairs(Reverse) do
+    Reverse[i].flags = gg.TYPE_DWORD
+    Reverse[i].value = '0'
+    Reverse[i].freeze = true
 end
+gg.addListItems(Reverse)
+gg.clearResults()
+gg.setVisible(false)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("67109633", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("67109633", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("67109633", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+local Reverse = gg.getResults(140701)
+for i, v in ipairs(Reverse) do
+    Reverse[i].flags = gg.TYPE_QWORD
+    Reverse[i].value = '0'
+    Reverse[i].freeze = true
+end
+gg.clearResults()
+gg.setVisible(false)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("134658", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("134658", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("134658", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+local Reverse = gg.getResults(140701)
+for i, v in ipairs(Reverse) do
+    Reverse[i].flags = gg.TYPE_DWORD
+    Reverse[i].value = '0'
+    Reverse[i].freeze = true
+end
+gg.addListItems(Reverse)
+gg.clearResults()
+gg.setVisible(false)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("134914", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("134914", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("134914", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+local Reverse = gg.getResults(140701)
+for i, v in ipairs(Reverse) do
+    Reverse[i].flags = gg.TYPE_DWORD
+    Reverse[i].value = '0'
+    Reverse[i].freeze = true
+end
+gg.addListItems(Reverse)
+gg.clearResults()
+gg.setVisible(false)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("70658", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("70658", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("70658", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+local Reverse = gg.getResults(140701)
+for i, v in ipairs(Reverse) do
+    Reverse[i].flags = gg.TYPE_DWORD
+    Reverse[i].value = '0'
+    Reverse[i].freeze = true
+end
+gg.clearResults()
+gg.setVisible(false)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("133378", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("133378", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("133378", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+local Reverse = gg.getResults(140701)
+for i, v in ipairs(Reverse) do
+    Reverse[i].flags = gg.TYPE_DWORD
+    Reverse[i].value = '0'
+    Reverse[i].freeze = true
+end
+gg.addListItems(Reverse)
+gg.clearResults()
+gg.setVisible(false)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("67109633", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("67109633", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("67109633", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+local Reverse = gg.getResults(140701)
+for i, v in ipairs(Reverse) do
+    Reverse[i].flags = gg.TYPE_QWORD
+    Reverse[i].value = '0'
+    Reverse[i].freeze = true
+end
+gg.clearResults()
+gg.setVisible(false)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("134658", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("134658", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("134658", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+local Reverse = gg.getResults(140701)
+for i, v in ipairs(Reverse) do
+    Reverse[i].flags = gg.TYPE_DWORD
+    Reverse[i].value = '0'
+    Reverse[i].freeze = true
+end
+gg.addListItems(Reverse)
+gg.clearResults()
+gg.setVisible(false)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("134914", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("134914", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("134914", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+local Reverse = gg.getResults(140701)
+for i, v in ipairs(Reverse) do
+    Reverse[i].flags = gg.TYPE_DWORD
+    Reverse[i].value = '0'
+    Reverse[i].freeze = true
+end
+gg.addListItems(Reverse)
+gg.clearResults()
+gg.setVisible(false)
+gg.clearResults()
+gg.sleep(1401)
+gg.clearResults()
+gg.setVisible(false)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("133378", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("133378", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("133378", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+local Reverse = gg.getResults(140701)
+for i, v in ipairs(Reverse) do
+    Reverse[i].flags = gg.TYPE_DWORD
+    Reverse[i].value = '0'
+    Reverse[i].freeze = true
+end
+gg.addListItems(Reverse)
+gg.clearResults()
+gg.setVisible(false)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("67109633", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("67109633", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("67109633", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+local Reverse = gg.getResults(140701)
+for i, v in ipairs(Reverse) do
+    Reverse[i].flags = gg.TYPE_QWORD
+    Reverse[i].value = '0'
+    Reverse[i].freeze = true
+end
+gg.clearResults()
+gg.setVisible(false)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("134658", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("134658", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("134658", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+local Reverse = gg.getResults(140701)
+for i, v in ipairs(Reverse) do
+    Reverse[i].flags = gg.TYPE_DWORD
+    Reverse[i].value = '0'
+    Reverse[i].freeze = true
+end
+gg.addListItems(Reverse)
+gg.clearResults()
+gg.setVisible(false)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("134914", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("134914", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("134914", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+local Reverse = gg.getResults(140701)
+for i, v in ipairs(Reverse) do
+    Reverse[i].flags = gg.TYPE_DWORD
+    Reverse[i].value = '0'
+    Reverse[i].freeze = true
+end
+gg.addListItems(Reverse)
+gg.clearResults()
+gg.setVisible(false)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("70658", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("70658", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("70658", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+local Reverse = gg.getResults(140701)
+for i, v in ipairs(Reverse) do
+    Reverse[i].flags = gg.TYPE_DWORD
+    Reverse[i].value = '0'
+    Reverse[i].freeze = true
+end
+gg.clearResults()
+gg.setVisible(false)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("133378", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("133378", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("133378", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+local Reverse = gg.getResults(140701)
+for i, v in ipairs(Reverse) do
+    Reverse[i].flags = gg.TYPE_DWORD
+    Reverse[i].value = '0'
+    Reverse[i].freeze = true
+end
+gg.addListItems(Reverse)
+gg.clearResults()
+gg.setVisible(false)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("67109633", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("67109633", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("67109633", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+local Reverse = gg.getResults(140701)
+for i, v in ipairs(Reverse) do
+    Reverse[i].flags = gg.TYPE_QWORD
+    Reverse[i].value = '0'
+    Reverse[i].freeze = true
+end
+gg.clearResults()
+gg.setVisible(false)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("134658", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("134658", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("134658", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+local Reverse = gg.getResults(140701)
+for i, v in ipairs(Reverse) do
+    Reverse[i].flags = gg.TYPE_DWORD
+    Reverse[i].value = '0'
+    Reverse[i].freeze = true
+end
+gg.addListItems(Reverse)
+gg.clearResults()
+gg.setVisible(false)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("134914", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("134914", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("134914", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+local Reverse = gg.getResults(140701)
+for i, v in ipairs(Reverse) do
+    Reverse[i].flags = gg.TYPE_DWORD
+    Reverse[i].value = '0'
+    Reverse[i].freeze = true
+end
+gg.addListItems(Reverse)
+gg.clearResults()
+gg.toast("Antiban Activated")
