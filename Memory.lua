e9657de5421ec94ed22c715994f9b3ef7619eff5
TLLSAIFUL <60128892+TLLSAIFUL@users.noreply.github.com>
2020-09-28 18:28:38 +0600

    B
    Bb

--- Memory.lua
+++ Memory.lua
@@ -1,84 +1,51 @@
-gg.setVisible(false)
 gg.clearResults()
-gg.setRanges(gg.REGION_C_ALLOC)
-gg.searchNumber("133378", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
-gg.searchNumber("133378", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
-gg.refineNumber("133378", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
-local Reverse = gg.getResults(140701)
-for i, v in ipairs(Reverse) do
-    Reverse[i].flags = gg.TYPE_DWORD
-    Reverse[i].value = '1337'
-    Reverse[i].freeze = true
+gg.setVisible(false)
+gg.setRanges(4)
+gg.searchNumber("133378", 4, false, 536870912, 0, -1)
+gg.searchNumber("133378", 4, false, 536870912, 0, -1)
+gg.refineNumber("133378", 4, false, 536870912, 0, -1)
+revert = gg.getResults(99999, nil, nil, nil, nil, nil, nil, nil, nil)
+local t = gg.getResults(99999, nil, nil, nil, nil, nil, nil, nil, nil)
+for i, v in ipairs(t) do
+	if v.flags == gg.TYPE_DWORD then
+		v.value = '"0"'
+		v.freeze = true
+	end
 end
-gg.addListItems(Reverse)
+gg.addListItems(t)
+t = nil
 gg.clearResults()
 gg.setVisible(false)
-gg.clearResults()
-gg.setRanges(gg.REGION_C_ALLOC)
-gg.searchNumber("67109633", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
-gg.searchNumber("67109633", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
-gg.refineNumber("67109633", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
-local Reverse = gg.getResults(140701)
-for i, v in ipairs(Reverse) do
-    Reverse[i].flags = gg.TYPE_QWORD
-    Reverse[i].value = '1337'
-    Reverse[i].freeze = true
+gg.setRanges(4)
+gg.searchNumber("134658", 4, false, 536870912, 0, -1)
+gg.searchNumber("134658", 4, false, 536870912, 0, -1)
+gg.refineNumber("134658", 4, false, 536870912, 0, -1)
+revert = gg.getResults(99999, nil, nil, nil, nil, nil, nil, nil, nil)
+local t = gg.getResults(99999, nil, nil, nil, nil, nil, nil, nil, nil)
+for i, v in ipairs(t) do
+	if v.flags == gg.TYPE_DWORD then
+		v.value = '"0"'
+		v.freeze = true
+	end
 end
+gg.addListItems(t)
+t = nil
 gg.clearResults()
 gg.setVisible(false)
-gg.clearResults()
-gg.setRanges(gg.REGION_C_ALLOC)
-gg.searchNumber("134658", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
-gg.searchNumber("134658", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
-gg.refineNumber("134658", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
-local Reverse = gg.getResults(140701)
-for i, v in ipairs(Reverse) do
-    Reverse[i].flags = gg.TYPE_DWORD
-    Reverse[i].value = '1337'
-    Reverse[i].freeze = true
+gg.setRanges(4)
+gg.searchNumber("131586", 4, false, 536870912, 0, -1)
+gg.searchNumber("131586", 4, false, 536870912, 0, -1)
+gg.refineNumber("131586", 4, false, 536870912, 0, -1)
+revert = gg.getResults(99999, nil, nil, nil, nil, nil, nil, nil, nil)
+local t = gg.getResults(99999, nil, nil, nil, nil, nil, nil, nil, nil)
+for i, v in ipairs(t) do
+	if v.flags == gg.TYPE_DWORD then
+		v.value = '"0"'
+		v.freeze = true
+	end
 end
-gg.addListItems(Reverse)
+gg.addListItems(t)
+t = nil
 gg.clearResults()
 gg.setVisible(false)
-gg.clearResults()
-gg.setRanges(gg.REGION_C_ALLOC)
-gg.searchNumber("134914", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
-gg.searchNumber("134914", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
-gg.refineNumber("134914", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
-local Reverse = gg.getResults(140701)
-for i, v in ipairs(Reverse) do
-    Reverse[i].flags = gg.TYPE_DWORD
-    Reverse[i].value = '1337'
-    Reverse[i].freeze = true
-end
-gg.addListItems(Reverse)
-gg.clearResults()
-gg.setVisible(false)
-gg.clearResults()
-gg.setRanges(gg.REGION_C_ALLOC)
-gg.searchNumber("70658", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
-gg.searchNumber("70658", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
-gg.refineNumber("70658", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
-local Reverse = gg.getResults(140701)
-for i, v in ipairs(Reverse) do
-    Reverse[i].flags = gg.TYPE_DWORD
-    Reverse[i].value = '1337'
-    Reverse[i].freeze = true
-end
-gg.clearResults()
-gg.setVisible(false)
-gg.clearResults()
-gg.setRanges(gg.REGION_C_ALLOC)
-gg.searchNumber("131586", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
-gg.searchNumber("131586", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
-gg.refineNumber("131586", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
-local Reverse = gg.getResults(140701)
-for i, v in ipairs(Reverse) do
-    Reverse[i].flags = gg.TYPE_DWORD
-    Reverse[i].value = '1337'
-    Reverse[i].freeze = true
-end
-gg.addListItems(Reverse)
-gg.clearResults()
-gg.setVisible(false)
-gg.toast("Antiban Activated")
+gg.alert("Antiban Activated"),
